d5964c61d48bbc68ec540ffa489332b64e44fbe6
Felipe Yasuo <felipe.yasuo@hotmail.com>
2025-09-27 23:33:21 -0300

    modificando firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCTDfAq-T4187uftjuKQOlD9wi4NMAEqyA",
     authDomain: "cadastro-web-d1b28.firebaseapp.com",
     projectId: "cadastro-web-d1b28",
-    storageBucket: "cadastro-web-d1b28.firebasestorage.app",
+    storageBucket: "cadastro-web-d1b28.appspot.com",
     messagingSenderId: "472723269927",
     appId: "1:472723269927:web:db7cbf5d76b4c7a711a102"
 };
